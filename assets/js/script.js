793c62476e8bddc754d5c4b4aa465bf76def4cb0
Luis Chavez <Dkangelas@gmail.com>
2021-12-16 19:14:43 -0600

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
+function getLanguage() {
+  var apikey = "6ab09c2d3be1e7f3ac8d2d8c4e31fe02"
+  var text = document.getElementById("#get-language").value;
+  console.log(text)
+  var url = "https://ws.detectlanguage.com/0.2/detect?q=" + text + "&key=" + apikey
+  fetch(url, {
+      method: "POST",
+      header: { "Content-Type": "application/json" }
+  })
+      .then(res => res.json())
+      .then(data => console.log(data))
+}
+getLanguage()
 
-var DetectLanguage = require('detectlanguage');
 
-var detectlanguage = new DetectLanguage('6ab09c2d3be1e7f3ac8d2d8c4e31fe02');
+function other(){
+    var textel = document.getElementById("get-language").value
+   
+}
+document.getElementsByClassName("translate-button").addEventListener("click", getLanguage()
 
-var text = "can you send";
 
-detectlanguage.detect(text).then(function(result) {
-  console.log(JSON.stringify(result));
-});
